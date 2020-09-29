d8ce88f3fb2a063b985ce31ffa5fca30ebb43a0d
mike.stackhouse <mike.stackhouse@atorusresearch.com>
2020-09-29 21:08:02 +0000

    Pushing installer stuff

---
+++ setup.sh
@@ -0,0 +1,9 @@
+# Download and install conda
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /tmp/miniconda.sh
+bash /tmp/miniconda.sh -b -p $HOME/miniconda
+
+# Using conda, install pandas
+$HOME/miniconda/bin/conda install pandas
+
+# Run package installation
+Rscript ./installpkgs.R
