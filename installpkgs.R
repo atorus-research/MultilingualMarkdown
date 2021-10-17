3ab2521750b50e605869ca6245ff1db7a36c2236
mike.stackhouse <mike.stackhouse@atorusresearch.com>
2021-10-17 18:41:00 +0000

    Redo setup scripts to avoid any rework or reinstallation

--- installpkgs.R
+++ installpkgs.R
@@ -1,8 +1,37 @@
-install.packages('rmarkdown', repos='cran.rstudio.com')
-install.packages('Rcpp', repos='cran.rstudio.com')
-install.packages('rprojroot', repos='cran.rstudio.com')
-install.packages('tidyverse', repos='cran.rstudio.com')
-install.packages('magrittr', repos='cran.rstudio.com')
-install.packages('knitr', repos='cran.rstudio.com')
-install.packages('kableExtra', repos='cran.rstudio.com')
-install.packages('reticulate', repos='cran.rstudio.com')
+#' Install packages if they're not already installed
+#' 
+#' This is a tiny wrapper to just make sure that the necessary
+#' packages are installed. We just want to make it easy to get the session
+#' ready for the workshop, so we don't want to run any rework if the necessary
+#' packages are already available.
+#' 
+#' **Use with caution** - this is not a robust function. This is only intended
+#' for use from a fresh slate like a new RStudio Cloud project. For example,
+#' if a dependency of a package is missing but the package is installed, this
+#' won't repair the missing dependency. 
+#'
+#' @param pkgs 
+#'
+#' @return NULL
+#' @examples
+#' 
+#' install_if_not(c('pharmaRTF', 'Tplyr'))
+#' 
+install_if_not <- function(pkgs) {
+   # Filter the list down to anything not installed
+   pkgs <- pkgs[!(pkgs %in% installed.packages())]
+   
+   if (length(pkgs) > 0) {
+      message('Installing the following packages:')
+      message(paste0("\t", pkgs, "\n"))
+   }
+   
+   install.packages(pkgs)
+}
+
+install_if_not(
+   c('rmarkdown', 'Rcpp', 'rprojroot', 'tidyverse', 
+     'magrittr', 'knitr', 'kableExtra', 'reticulate')
+)
+
+system('bash ./setup.sh')
--- setup.sh
+++ setup.sh
@@ -1,9 +1,13 @@
-# Download and install conda
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /tmp/miniconda.sh
-bash /tmp/miniconda.sh -b -p $HOME/miniconda
+#!/bin/bash
 
-# Using conda, install pandas
-$HOME/miniconda/bin/conda install -y pandas
+if [ ! -d "$HOME/miniconda" ]
+then 
+   # Download and install conda
+   wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /tmp/miniconda.sh
+   bash /tmp/miniconda.sh -b -p $HOME/miniconda
 
-# Run package installation
-Rscript ./installpkgs.R
+   # Using conda, install pandas
+   $HOME/miniconda/bin/conda install -y pandas
+else
+   echo "minconda is already installed"
+fi
