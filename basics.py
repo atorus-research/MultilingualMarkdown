b1a07a978c297e268af6895ca5b5ea78f722f568
Nathan Kosiba <nathan.kosiba@atorusresearch.com>
2021-10-22 17:37:37 +0000

    add basics.py file

---
+++ basics.py
@@ -0,0 +1,49 @@
+# basic console output
+print("Hello world")
+
+# console output using variables
+x = 1
+print(x)
+print(x + 1)
+
+
+# erroring and variable types
+x[1]
+type(x)
+
+# for loops
+for i in range(1,11):
+print(i)
+
+
+for i in range(1,11):
+   print(i)
+   
+   
+for i in range(1,11):
+   if i % 2 == 0:
+      print('even')
+   elif i == 5:
+      print('FIVE!')
+   else:
+      print('odd')
+
+# working with strings
+x = "Hello"
+y = "Goodbye"
+x + y
+
+'el' in "Hello"
+'Hello'.find('el')
+"Hello"[2:4]
+
+# indexing
+x = [1, 2, 3, 4, 5]
+x[2]
+x[0]
+
+
+
+
+
+
