69952f5ea7328617ab8625b9774eff94179a509d
mike.stackhouse <mike.stackhouse@atorusresearch.com>
2021-10-22 20:40:10 +0000

    Comment updates

--- basics.py
+++ basics.py
@@ -11,15 +11,16 @@ print(x + 1)
 x[1]
 type(x)
 
-# for loops
+# for loops - this will error because there's no indentation within the loop
 for i in range(1,11):
 print(i)
 
-
+# This will properly print 1 to 10
 for i in range(1,11):
    print(i)
    
-   
+
+# This loop adds if conditions evaluated through each iteration of the loop
 for i in range(1,11):
    if i % 2 == 0:
       print('even')
@@ -33,8 +34,13 @@ x = "Hello"
 y = "Goodbye"
 x + y
 
+# Check if the string 'el' is a substring within "Hello"
 'el' in "Hello"
+
+# Check where "el" appears within "Hello"
 'Hello'.find('el')
+
+# Return the 3rd and 4th character within "Hello"
 "Hello"[2:4]
 
 # indexing
